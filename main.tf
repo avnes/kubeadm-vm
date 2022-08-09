59cd4d7e7a01103fe9bf02daddd9fa22595500e0
Audun V. Nes <audun.nes@gmail.com>
2022-08-09 22:43:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,4 @@ module "kubeadm_vm" {
     source                  = "github.com/avnes/terraform-libvirt-vm"
     project_name            = var.kubeadm_vm_project_name
     nodes                   = var.kubeadm_vm_nodes
-    cloud_image_base_url    = var.kubeadm_vm_cloud_image_base_url
-    cloud_image_name        = var.kubeadm_vm_cloud_image_name
 }
